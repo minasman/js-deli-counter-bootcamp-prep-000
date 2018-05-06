3571e5281005c35cdad9e24c99770ff3021f765d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 20:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
-    var name = katzDeliLine[0];
-    katzDeliLine.shift();
-    return `Currently serving ${name}.`;
+    //var name = katzDeliLine[0];
+    //katzDeliLine.shift();
+    return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return 'There is nobody waiting to be served!';
   }
